badb90edc07c0267da55f57ef6995c841a32e2f5
Najam <hmnajam@gmail.com>
2024-08-09 03:11:48 +0500

    Removed poetry and added pip because Vercel doesnt support poetry

--- api/index.py
+++ api/index.py
@@ -3,10 +3,20 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+# app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
+
+
 @app.get("/api/python")
 def hello_world():
-    return {"message": "Hello World"}
+    return {"message": "Hello from GenAI SaaS Boilerplate."}
 
 
-# poetry run uvicorn index:app 
+# poetry run uvicorn index:app
 # "fastapi-dev": "pip3 install -r requirements.txt && python3 -m uvicorn api.index:app --reload",
+
+
+# Run the server: poetry run uvicorn crypto_trading_bot.main:app --reload --host 0.0.0.0 --port 8000
+# Open the following urls:
+# 	- http://localhost:8000
+# 	- http://localhost:8000/docs
+# 	- http://localhost:8000/openapi.json
