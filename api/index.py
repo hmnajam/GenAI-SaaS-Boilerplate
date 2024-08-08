8f66ff10b11f0c4a0a072fab1aa72b20ba778b92
Najam <hmnajam@gmail.com>
2024-08-09 03:36:05 +0500

    Added SQLModel

--- api/index.py
+++ api/index.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 @app.get("/api/python")
 def hello_world():
-    return {"message": "Hello from GenAI SaaS Boilerplate."}
+    return {"message": "Hi from GenAI SaaS Boilerplate."}
 
 
 # poetry run uvicorn index:app
